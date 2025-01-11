771eb79e6312bae084b55b19416ac8c3f7df606f
Nest <58371847+Nest4u@users.noreply.github.com>
2025-01-11 21:37:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { TodoList } from "./components/TodoList";
 
 const App: React.FC = () => {
   return (
-    <TodoProvider>
+  
       <div className="text-center bg-gray-500 min-h-screen flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-4">Todo List</h1>
         <AddTodo />
         <TodoList  />
       </div>
-    </TodoProvider>
+   
   );
 };
 
