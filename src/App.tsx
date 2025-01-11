a05217d8e98ed0b019ae1b007283ecc2de1ccf50
Nest <58371847+Nest4u@users.noreply.github.com>
2025-01-11 21:35:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TodoProvider } from "./context/TodoContext";
+
 import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
 
